374e5d0f6377fc6ca4e3fa0ca1ebae1d7e108135
xarsin <jdj.souza@gmail.com>
2020-10-09 16:32:46 -0500

    collect data from input fields and push to array

--- scripts/client.js
+++ scripts/client.js
@@ -1,5 +1,29 @@
 $(document).ready(onReady);
+const employeeList = [];
 
 function onReady() {
   console.log('JQuery is Loaded');
+
+  $('.js-click-submit').on('click', submitEmployee);
+}
+
+function submitEmployee() {
+  const firstName = $('.js-field-first-name').val();
+  const lastName = $('.js-field-last-name').val();
+  const id = $('.js-field-id').val();
+  const title = $('.js-field-title').val();
+  const salary = $('.js-field-salary').val();
+  const employee = {
+    firstName,
+    lastName,
+    id,
+    title,
+    salary,
+  };
+  employeeList.push(employee);
+  output();
+}
+
+function output() {
+  console.log(employeeList);
 }
